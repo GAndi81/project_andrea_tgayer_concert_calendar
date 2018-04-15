05b3681116987113b3885a0664b306918ebd0caf
Andrea Tóth-Gayer <gayerandi@yahoo.com>
2018-04-15 21:54:39 +0200

    Feat: updates on backend

--- Backend/app.js
+++ Backend/app.js
@@ -4,22 +4,28 @@ const app = express();
 
 
 //3500-as porton figyel a server
-app.listen(3500, () => console.log('Example app listening on port 3500!'))
+app.listen(3500, () => console.log('Example app listening on port 3500!'));
 
 //üzenet a 3500-as portra
-app.get('/', (req, res) => res.send('Hello Family!'))
+app.get('/', (req, res) => res.send('Hello Family!'));
 
 // válasz egy POST requestre
 app.post('/', (req, res) => {
     res.send('Got a POST request')
-})
+});
 
 //válasz egy PUT requestre /user routnál
 app.put('/user', function (req, res) {
     res.send('Got a PUT request at /user')
-})
+});
 
 //válasz egy DELETE requestre /user routnál
 app.delete('/user', function (req, res) {
     res.send('Got a DELETE request at /user')
-})
+});
+
+let users = require('./users');
+app.use('/users', users);
+
+let concertcalendar = require('./concertcalendar');
+app.use('/concertcalendar', concertcalendar);
