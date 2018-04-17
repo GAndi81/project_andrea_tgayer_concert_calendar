31c8fe252276cf343edc8d19d6e22e1775db3695
Andrea Tóth-Gayer <gayerandi@yahoo.com>
2018-04-17 13:59:23 +0200

    Frontend - component/pug

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { environment } from '../environments/environment';
 import { RouterModule, Routes } from '@angular/router';
 
 
